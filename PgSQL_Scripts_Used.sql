ed592f4bdaaa4bf35a2d1959dda52a658b51fbb8
pandyamehul <pandya.mehul@live.com>
2021-02-08 00:43:58 +0100

    Updated Ef code migration for SIT env.

--- PgSQL_Scripts_Used.sql
+++ PgSQL_Scripts_Used.sql
@@ -1,4 +1,7 @@
+-- Local Dev
 create user cmddbuser with encrypted password 'pa55w0rd!' createdb;
+-- SIT
+create user cmddbuser with encrypted password 'pa55w0rd!_dev' createdb;
 
 insert into "CommandItems" ("HowTo", "Platform", "CommandLine") values ('Create an EF migration', 'Entity Framework Core Command Line', 'dotnet ef migrations add');
 insert into "CommandItems" ("HowTo", "Platform", "CommandLine") VALUES ('Apply Migrations to DB', 'Entity Framework Core Command Line', 'dotnet ef database update');
