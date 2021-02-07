53b9062edd412bc3a8f8b948f112bda87272c435
pandyamehul <pandya.mehul@live.com>
2021-02-07 15:52:06 +0100

    Added operation POST, PUT operation with automated unit test cases using TDD

--- CommandAPI.Tests/CommandsControllerTests.cs
+++ CommandAPI.Tests/CommandsControllerTests.cs
@@ -177,5 +177,131 @@ namespace CommandAPI.Tests
                //Assert
                Assert.Equal(cmdId, result.Value.Id);               
           }
+          [Fact]
+          public void PostCommandItemObjectCountIncrementWhenValidObject()
+          {
+               //Action 3 Test : POST /api/commands : Create a new resource
+               //Test# 3.1 : Condition = Valid Object Submitted for Creation : Result = Object count increments by 1
+
+               //Arrange
+               var command = new Command {
+                    HowTo = "Do unit test",
+                    Platform = "xUNit Platform",
+                    CommandLine = "dotnet test"
+               };
+               var oldCount = _dbContext.CommandItems.Count();
+               //Act
+               var result = _controller.PostCommandItem(command);
+               //Assert
+               Assert.Equal(oldCount+1, _dbContext.CommandItems.Count());
+          }
+          [Fact]
+          public void PostCommandItemReturns201CreatedWhenValidObject()
+          {
+               //Action 3 Test : POST /api/commands : Create a new resource
+               //Test# 3.2 : Condition = Valid Object Submitted for Creation 201 : Result = Created Return Code
+
+               //Arrange
+               var command = new Command {
+                    HowTo = "Do unit test",
+                    Platform = "xUNit Platform",
+                    CommandLine = "dotnet test"
+               };
+               var oldCount = _dbContext.CommandItems.Count();
+               //Act
+               var result = _controller.PostCommandItem(command);
+               //Assert
+               Assert.IsType<CreatedAtActionResult>(result.Result);
+          }
+          [Fact]
+          public void PutCommandItem_AttributeUpdated_WhenValidObject()
+          {
+               //Action 4 Test : PUT /api/commands/{Id} : Update Update a single resource, (by Id)
+               //Test# 4.1 : Condition = Valid Object Submitted for Update : Result = Attribute is updated
+
+               //Arrange
+               var _command = new Command {
+                    HowTo = "Do unit test",
+                    Platform = "xUNit Platform",
+                    CommandLine = "dotnet test"
+               };
+               _dbContext.CommandItems.Add(_command);
+               _dbContext.SaveChanges();
+               int cmdId = _command.Id;
+               _command.HowTo = "dotnet test - updated";
+
+               //Act
+               _controller.PutCommandItem(cmdId, _command);
+               var result = _dbContext.CommandItems.Find(cmdId);
+               //Assert
+               Assert.Equal(_command.HowTo, result.HowTo);
+          }
+          [Fact]
+          public void PutCommandItem_Returns204_WhenValidObject()
+          {
+               //Action 4 Test : PUT /api/commands/{Id} : Update Update a single resource, (by Id)
+               //Test# 4.2 : Condition = Valid Object Submitted for Update 204 : Result = No Content Return Code
+
+               //Arrange
+               var _command = new Command {
+                    HowTo = "Do unit test",
+                    Platform = "xUNit Platform",
+                    CommandLine = "dotnet test"
+               };
+               _dbContext.CommandItems.Add(_command);
+               _dbContext.SaveChanges();
+               int cmdId = _command.Id;
+               _command.HowTo = "dotnet test - updated";
+               //Act
+               var result = _controller.PutCommandItem(cmdId, _command);
+               //Assert
+               Assert.IsType<NoContentResult>(result);
+          }
+          [Fact]
+          public void PutCommandItem_Returns400_WhenInvalidObject()
+          {
+               //Action 4 Test : PUT /api/commands/{Id} : Update Update a single resource, (by Id)
+               //Test# 4.3 : Condition = Invalid Object Submitted for Update 400 : Result = Bad Request Return Code
+
+               //Arrange
+               var _command = new Command {
+                    HowTo = "Do unit test",
+                    Platform = "xUNit Platform",
+                    CommandLine = "dotnet test"
+               };
+               _dbContext.CommandItems.Add(_command);
+               _dbContext.SaveChanges();
+               int cmdId = _command.Id + 1;
+               _command.HowTo = "dotnet test - updated";
+               //Act
+               var result = _controller.PutCommandItem(cmdId, _command);
+               //Assert
+               Assert.IsType<BadRequestResult>(result);
+          }
+          [Fact]
+          public void PutCommandItem_AttributeUnchanged_WhenInvalidObject()
+          {
+               //Action 4 Test : PUT /api/commands/{Id} : Update Update a single resource, (by Id)
+               //Test# 4.4 : Condition = Invalid Object Submitted for Update : Result = Object remains unchanged
+
+               //Arrange
+               var _command1 = new Command {
+                    HowTo = "Do unit test",
+                    Platform = "xUNit Platform",
+                    CommandLine = "dotnet test"
+               };
+               var _command2 = new Command {
+                    HowTo = "Do unit test - UPDATED",
+                    Platform = "xUNit Platform - UPDATED",
+                    CommandLine = "dotnet test - UPDATED"
+               };               
+               _dbContext.CommandItems.Add(_command1);
+               _dbContext.SaveChanges();
+               //Act
+               _controller.PutCommandItem(_command1.Id + 1, _command2);
+               var result = _dbContext.CommandItems.Find(_command1.Id);
+               //Assert
+               Assert.Equal(_command1.HowTo, result.HowTo);
+          }                   
      }
 }
